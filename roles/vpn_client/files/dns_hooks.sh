45d9acdd0f23c7f3a62f0f9b99d9e3dc362b8c25
koffcheg <kharkov501@gmail.com>
2025-06-17 02:59:52 +0300

    update uploading of right archive

--- roles/vpn_client/files/dns_hooks.sh
+++ roles/vpn_client/files/dns_hooks.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-# /etc/openvpn/dns-hooks.sh
+# /etc/openvpn/client/dns-hooks.sh
 # Resilient DNS hook for OpenVPN clients
 
 LOG=/tmp/openvpn-dns.log
 echo "$(date): DNS hook $script_type started" >> $LOG
 
 if [ "$script_type" = "up" ]; then
-    if [ -x /etc/openvpn/update-resolv-conf ]; then
-        exec /etc/openvpn/update-resolv-conf "$@"
-    elif [ -x /etc/openvpn/update-systemd-resolved ]; then
-        exec /etc/openvpn/update-systemd-resolved "$@"
+    if [ -x /etc/openvpn/client/update-resolv-conf ]; then
+        exec /etc/openvpn/client/update-resolv-conf "$@"
+    elif [ -x /etc/openvpn/client/update-systemd-resolved ]; then
+        exec /etc/openvpn/client/update-systemd-resolved "$@"
     else
         echo "$(date): No DNS updater found" >> $LOG
     fi
 elif [ "$script_type" = "down" ]; then
-    if [ -x /etc/openvpn/update-resolv-conf ]; then
-        exec /etc/openvpn/update-resolv-conf "$@"
-    elif [ -x /etc/openvpn/update-systemd-resolved ]; then
-        exec /etc/openvpn/update-systemd-resolved "$@"
+    if [ -x /etc/openvpn/client/update-resolv-conf ]; then
+        exec /etc/openvpn/client/update-resolv-conf "$@"
+    elif [ -x /etc/openvpn/client/update-systemd-resolved ]; then
+        exec /etc/openvpn/client/update-systemd-resolved "$@"
     else
         echo "$(date): No DNS cleanup found" >> $LOG
     fi
